e2828b14e05c66b3acef08f426140594db45fdca
assad <assadullah.ghaznawi@gmail.com>
2020-04-16 16:40:49 +0200

    version 2

---
+++ FileReaderHandler/XmlReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReaderHandler
+{
+    class XmlReader : FileReaderHandler
+    {
+        XmlReader(string path) : base(path)
+        {
+
+        }
+
+        public new string ReadFile()
+        {
+            return ReadFile();
+        }
+    }
+}
