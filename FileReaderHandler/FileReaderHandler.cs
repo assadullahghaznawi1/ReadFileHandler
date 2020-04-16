41d7136023c8d36d3281ceef2c057b520211d9d1
assad <assadullah.ghaznawi@gmail.com>
2020-04-16 16:36:33 +0200

    version 1

---
+++ FileReaderHandler/FileReaderHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileReaderHandler
+{
+    public enum FileType
+    {
+        Text
+    }
+    public class FileReaderHandler
+    {
+        protected string Path { get; set; }
+        public FileReaderHandler(string path)
+        {
+            Path = path;
+        }
+
+        protected string ReadFile()
+        {
+            try
+            {
+                StreamReader streamReader = new StreamReader(Path);
+                string result = streamReader.ReadToEnd();
+                streamReader.Close();
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                return ("Error " + e.Message);
+            }
+        }
+    }
+}
---
+++ FileReaderHandler/TextReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReaderHandler
+{
+    class TextReader : FileReaderHandler
+    {
+        TextReader(string path) : base(path)
+        {
+
+        }
+
+        public new string ReadFile()
+        {
+            return ReadFile();
+        }
+    }
+}
